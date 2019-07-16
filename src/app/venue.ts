2fdc84a2a11427907d1b7380dcc1ba023a17b9c9
Saami PERDRIX <saami.perdrix@cgi.com>
2019-07-16 11:34:04 +0200

    class venue

---
+++ src/app/venue.ts
@@ -0,0 +1,9 @@
+export class Venue {
+    yelp_id: string;
+    name: string;
+    espacePoussette: boolean;
+    tableAlanger: boolean;
+    tableAlangerMr: boolean;
+    menuEnfant: boolean;
+    jeuxEnfant: boolean;
+}
