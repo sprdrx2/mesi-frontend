9550ceb6c7eab0826c3add654abbc2c8521d8c0c
Saami PERDRIX <saami@perdrix.xyz>
2019-08-03 14:35:06 +0200

    recupere tous les resultats de yelp

--- src/app/yelp.service.ts
+++ src/app/yelp.service.ts
@@ -14,23 +14,20 @@ export class YelpService {
   private yelpApiKey: string = 'VUgnEj3HGTTbpvVzNh_gChrFdhnn9Gw75jKm761Hlel0tzsF57f3jdptFHQEtO5C7pBjzndUmIcv0S1C7eZh_-9TCI5m5JKVqwB7rFCQDu1ztwvwxjK1Sqs6OJQsXXYx';
   yelpVenues: Array<YelpVenue>;
   yelpTotal: Number;
- 
-
 
   async getVenues(location: String) {
     const authHeaders = new HttpHeaders().set("Authorization", "Bearer " + this.yelpApiKey);
     await this.httpClient.get<YelpResponse>(this.yelpApi + '?limit=50&radius=2000&categories=bars,restaurant&location=' + location, { headers: authHeaders }).toPromise().then((r: YelpResponse) => { this.yelpTotal = r.total; this.yelpVenues = r.businesses; });
     console.log('Yelp: ' + this.yelpTotal + ' results:' );
-    var offset = 1; const promises = [];
-    /*while(this.yelpOffset <= this.yelpTotal && this.yelpOffset <= 1000) {
-        offset = offset + 50;
-	const promise = this.httpClient.get<YelpResponse>(this.yelpApi + '?offset='+ offset +'&limit=50&radius=2000&categories=bars,restaurant&location=' + location, { headers: authHeaders }).toPromise();
-	promises.push(promise);	
+    let offset = 51;
+    while (offset <= this.yelpTotal && offset <= 1000 ) {
+      await this.httpClient.get<YelpResponse>(this.yelpApi + '?offset=' + offset + '&limit=50&radius=2000&categories=bars,restaurant&location=' + location, { headers: authHeaders }).toPromise().then((r: YelpResponse) => { this.yelpVenues = this.yelpVenues.concat(r.businesses); });
+      offset = offset + 50;
     }
-    await Promise.all(promises);*/
     console.log(this.yelpVenues);
-    return this.yelpVenues;  
+    return this.yelpVenues;
   }
+
 
   constructor(private httpClient: HttpClient) { }
 }
